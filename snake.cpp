2f8663d9565c027ee3bb6085d62717f42b775449
Teo <69358538+teoexe@users.noreply.github.com>
2022-05-24 22:54:41 +0200

    Update snake.cpp

--- snake.cpp
+++ snake.cpp
@@ -2,10 +2,6 @@
 ** Chiara Signani
 ** snake.cpp
 ** 18/05/2022
-
-risolvi errori in funzioni inizializza bonus/malus
-togliere i vettori che memorizzano le posizioni bonus/malus
-
 */
 
 #include <iostream>
@@ -16,7 +12,9 @@ togliere i vettori che memorizzano le posizioni bonus/malus
 #define BONUS 5
 #define MALUS 5
 #define VOID_ELEMENT -1
-#define RETURN_ERROR -1
+#define MALUS_HIT -2
+#define ATE_YOURSELF -1
+#define WON 0
 using namespace std;
 
 class snake {
@@ -89,58 +87,90 @@ void resetSnake(char m[DIM][DIM]) {
     }
 }
 
+void moveSnake(int length_idx) {
+    for(int i = length_idx - 1; i > 0; i--) length[i].setpos(length[i - 1].x,length[i - 1].y);
+}
+
 int main() {
     srand(time(NULL));
     for(int i = 0; i < DIM * DIM; i++) length[i].reset();
     int length_idx = 1;
     length[0].setpos(0,0);
-    char m[DIM][DIM], snake='O', comando, sn;
+    char m[DIM][DIM], snake='O', sn;
     int x = 0, y = 0, punti = 0;
 
-    cout << "Istruzioni del gioco: " << endl;
-    cout << "1. Utilizza il tasto 'w' per spostarti verso l'alto " << endl;
-    cout << "2. Utilizza il tasto 's' per spostarti verso il basso " << endl;
-    cout << "3. Utilizza il tasto 'd' per spostarti verso destra " << endl;
+    cout << "Istruzioni del gioco:" << endl;
+    cout << "1. Utilizza il tasto 'w' per spostarti verso l'alto" << endl;
+    cout << "2. Utilizza il tasto 's' per spostarti verso il basso" << endl;
+    cout << "3. Utilizza il tasto 'd' per spostarti verso destra" << endl;
     cout << "4. Utilizza il tasto 'a' per spostarti verso sinistra" << endl;
-    cout << "5. Utilizza il tasto 'm' per uscire dal gioco " << endl;
-    cout << "6. Cerca di prendere tutti i bonus (+) per vincere " << endl;
-    cout << "7. Cerca di evitare i malus (x) per non perdere " << endl;
+    cout << "5. Utilizza il tasto 'm' per uscire dal gioco" << endl;
+    cout << "6. Cerca di prendere tutti i bonus (+) per vincere" << endl;
+    cout << "7. Cerca di evitare i malus (x) per non perdere" << endl;
     cout << "Premi un tasto per continuare" << endl;
     getch();
 
     inizializza_matrice(m,'.');
     init_bonus(m);
     init_malus(m);
-
-    while (true) {
-        resetSnake(m);
-        for(int i = 0; i < length_idx; i++) length[i].setOnMatrix(m);
-        system("cls");
-        stampa_matrice(m);
-        cout << "Score: " << punti << "/" << BONUS << endl;
-        comando = getch();
-        switch(comando) {
-        case('w'):
-            if(m[length[0].x - 1][length[0].y] == '+') {
-                punti++;
-                length_idx++;
-                length[length_idx - 1].setpos(length[0].x,length[0].y);
+    try {
+        while (true) {
+            resetSnake(m);
+            for(int i = 0; i < length_idx; i++) length[i].setOnMatrix(m);
+            system("cls");
+            stampa_matrice(m);
+            cout << "Score: " << punti << "/" << BONUS << endl;
+            if(punti == 5) throw(WON);
+            switch(getch()) {
+                case('w'):
+                if(m[length[0].x - 1][length[0].y] == '+') {
+                    punti++;
+                    length_idx++;
+                    length[length_idx - 1].setpos(length[0].x,length[0].y);
+                } else if(m[length[0].x - 1][length[0].y] == 'O') throw(ATE_YOURSELF);
+                else if(m[length[0].x - 1][length[0].y] == 'x') throw(MALUS_HIT);
+                moveSnake(length_idx);
+                length[0].x -= 1;
+                break;
+                case('a'):
+                if(m[length[0].x][length[0].y - 1] == '+') {
+                    punti++;
+                    length_idx++;
+                    length[length_idx - 1].setpos(length[0].x,length[0].y);
+                } else if(m[length[0].x][length[0].y - 1] == 'O') throw(ATE_YOURSELF);
+                else if(m[length[0].x][length[0].y - 1] == 'x') throw(MALUS_HIT);
+                moveSnake(length_idx);
+                length[0].y -= 1;
+                break;
+                case('s'):
+                if(m[length[0].x + 1][length[0].y] == '+') {
+                    punti++;
+                    length_idx++;
+                    length[length_idx - 1].setpos(length[0].x,length[0].y);
+                } else if(m[length[0].x + 1][length[0].y] == 'O') throw(ATE_YOURSELF);
+                else if(m[length[0].x + 1][length[0].y] == 'x') throw(MALUS_HIT);
+                moveSnake(length_idx);
+                length[0].x += 1;
+                break;
+                case('d'):
+                if(m[length[0].x][length[0].y + 1] == '+') {
+                    punti++;
+                    length_idx++;
+                    length[length_idx - 1].setpos(length[0].x,length[0].y);
+                } else if(m[length[0].x][length[0].y + 1] == 'O') throw(ATE_YOURSELF);
+                else if(m[length[0].x][length[0].y + 1] == 'x') throw(MALUS_HIT);
+                moveSnake(length_idx);
+                length[0].y += 1;
+                break;
+                default:
+                break;
             }
-            length[0].x -= 1;
-            break;
-        case('a'):
-            length[0].y -= 1;
-            break;
-        case('s'):
-            length[0].x += 1;
-            break;
-        case('d'):
-            length[0].y += 1;
-            break;
-        default:
-            break;
         }
-    //no
+    }
+    catch(int end_status) {
+        if(end_status == ATE_YOURSELF) cout << "Hai mangiato la coda del tuo serpente!";
+        else if(end_status == MALUS_HIT) cout << "Hai colpito un malus!";
+        else cout << "Hai vinto!";
     }
     return 0;
 }
